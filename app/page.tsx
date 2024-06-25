c3b5743bf5151de68137afe3587c8e7c7ad27d80
emanuelefavero <emafaux89@gmail.com>
2024-06-25 22:19:42 +0200

    udpate text

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,14 @@ export default function Home() {
     <>
       <Header />
 
-      <main className='px-2 py-2'>
-        <Typography variant='h1'>Shadcn/ui</Typography>
+      <main className='px-4 py-2'>
+        <Typography variant='h1' className='mb-4'>
+          Title
+        </Typography>
+        <Typography variant='p' className='mb-4 text-muted-foreground'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
+          ducimus at nam.
+        </Typography>
 
         <Button>Click me</Button>
       </main>
--- components/ui/typography.tsx
+++ components/ui/typography.tsx
@@ -10,8 +10,8 @@ export const typographyVariants = cva('text-xl', {
       h3: 'scroll-m-20 text-2xl font-semibold tracking-tight',
       h4: 'scroll-m-20 text-xl font-semibold tracking-tight',
       p: 'leading-7 [&:not(:first-child)]:mt-6',
-      // blockquote: "mt-6 border-l-2 pl-6 italic",
-      // list: "my-6 ml-6 list-disc [&>li]:mt-2",
+      blockquote: 'mt-6 border-l-2 pl-6 italic',
+      // list: 'my-6 ml-6 list-disc [&>li]:mt-2',
     },
     affects: {
       default: '',
