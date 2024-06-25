1575b25cd8760733086ad97d5f2be01d86d0f1ff
emanuelefavero <emafaux89@gmail.com>
2024-06-25 22:15:05 +0200

    add typography

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { Button } from '@/components/ui/button'
+import { Typography } from '@/components/ui/typography'
 import Header from '@/components/header'
 
 export default function Home() {
@@ -7,7 +8,7 @@ export default function Home() {
       <Header />
 
       <main className='px-2 py-2'>
-        <h1>Shadcn/ui</h1>
+        <Typography variant='h1'>Shadcn/ui</Typography>
 
         <Button>Click me</Button>
       </main>
