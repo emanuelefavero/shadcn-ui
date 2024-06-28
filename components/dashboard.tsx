97fecd92417eaeaf5c3a995c99d69475620ce1b1
emanuelefavero <emafaux89@gmail.com>
2024-06-28 15:41:17 +0200

    add search-input

--- components/dashboard.tsx
+++ components/dashboard.tsx
@@ -1,10 +1,14 @@
 import { H1 } from '@/components/ui/typography'
-import { MainCard } from './main-card'
+import { MainCard } from '@/components/main-card'
+import { SearchInput } from '@/components/search-input'
 
 function Dashboard() {
   return (
     <>
-      <H1 className='font-semibold text-3xl mb-10 select-none'>Dashboard</H1>
+      <section className='flex justify-between'>
+        <H1 className='font-semibold text-3xl mb-10 select-none'>Dashboard</H1>
+        <SearchInput />
+      </section>
 
       <MainCard />
     </>
---
+++ components/search-input.tsx
@@ -0,0 +1,7 @@
+import { Input } from '@/components/ui/input'
+
+function SearchInput() {
+  return <Input type='search' placeholder='Search' className='max-w-60'></Input>
+}
+
+export { SearchInput }
