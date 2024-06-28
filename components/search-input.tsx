dacb8d513c515f7fc28378bf402dbaca8b725a1b
emanuelefavero <emafaux89@gmail.com>
2024-06-28 16:00:37 +0200

    add icon inside search input

--- components/search-input.tsx
+++ components/search-input.tsx
@@ -1,7 +1,17 @@
 import { Input } from '@/components/ui/input'
+import { Search } from 'lucide-react'
 
 function SearchInput() {
-  return <Input type='search' placeholder='Search' className='max-w-60'></Input>
+  return (
+    <div className='relative max-w-60'>
+      <Input
+        type='search'
+        placeholder='Search...'
+        className='w-full pl-9 pr-4 py-2 bg-purple-900 bg-opacity-5 placeholder:text-slate-500'
+      />
+      <Search className='h-5 w-5 absolute left-2 top-2.5 text-slate-500' />
+    </div>
+  )
 }
 
 export { SearchInput }
