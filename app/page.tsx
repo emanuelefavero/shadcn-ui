d4ec3cd7e532ea94c8bf4887de977e429e594d53
emanuelefavero <emafaux89@gmail.com>
2024-06-28 03:35:56 +0200

    add react-animated-cursor

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { Header } from '@/components/header'
 import { Dashboard } from '@/components/dashboard'
+import AnimatedCursor from 'react-animated-cursor'
 
 export default function Home() {
   return (
@@ -9,6 +10,15 @@ export default function Home() {
       <main className='px-4 py-2'>
         <Dashboard />
       </main>
+
+      <AnimatedCursor
+        innerSize={8}
+        outerSize={8}
+        color='168, 85, 247'
+        outerAlpha={0.2}
+        innerScale={0.5}
+        outerScale={4}
+      />
     </>
   )
 }
