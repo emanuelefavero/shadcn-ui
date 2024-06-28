0cab5480da70641ffc543cffeefbcd739dd1ff45
emanuelefavero <emafaux89@gmail.com>
2024-06-28 20:49:19 +0200

    change hover background color on search menu and sidebar

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -17,7 +17,7 @@ const buttonVariants = cva(
         secondary:
           'bg-secondary text-secondary-foreground hover:bg-secondary/80',
         ghost:
-          'hover:bg-purple-500 hover:bg-opacity-25 hover:text-accent-foreground',
+          'hover:bg-purple-500 hover:bg-opacity-10 hover:text-accent-foreground',
         link: 'text-primary underline-offset-4 hover:underline',
       },
       size: {
