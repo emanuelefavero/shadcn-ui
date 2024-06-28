a3ac1fb14c470933e466d132804a568ae224902f
emanuelefavero <emafaux89@gmail.com>
2024-06-28 02:33:03 +0200

    move main data to separate file

--- components/first-card.tsx
+++ components/first-card.tsx
@@ -1,43 +1,18 @@
+'use client'
+
+import { useState } from 'react'
 import { EllipsisVertical } from 'lucide-react'
 import { Card } from '@/components/ui/card'
 import { Badge } from '@/components/ui/badge'
 import { convertLabelToId } from '@/utils/stringUtils'
-
-const numbersData = [
-  {
-    label: 'Visitors',
-    value: 30794,
-    formattedValue: '30,794',
-    percentage: 22,
-    selected: true,
-  },
-  {
-    label: 'Contacts',
-    value: 1983,
-    formattedValue: '1,983',
-    percentage: 21,
-    selected: false,
-  },
-  {
-    label: 'Attributable Deals',
-    value: 57,
-    formattedValue: '57',
-    percentage: 12.6,
-    selected: false,
-  },
-  {
-    label: 'Revenue',
-    value: 10622.21,
-    formattedValue: '$10,622.21',
-    percentage: 15.2,
-    selected: false,
-  },
-]
+import { mainData } from '@/data/mainData'
 
 function FirstCard() {
+  const [data, setData] = useState(mainData)
+
   return (
     <Card className='grid grid-cols-[repeat(2,1fr)] max-w-[500px] w-full rounded-xl overflow-hidden bg-purple-900 bg-opacity-5 [&>*:nth-child(odd)]:border-r [&>*:nth-child(1)]:border-b [&>*:nth-child(2)]:border-b [&>*]:border-border'>
-      {numbersData.map((data) => (
+      {data.map((data) => (
         <div
           key={data.label}
           className={`flex flex-col p-5 ${
---
+++ data/mainData.ts
@@ -0,0 +1,30 @@
+export const mainData = [
+  {
+    label: 'Visitors',
+    value: 30794,
+    formattedValue: '30,794',
+    percentage: 22,
+    selected: true,
+  },
+  {
+    label: 'Contacts',
+    value: 1983,
+    formattedValue: '1,983',
+    percentage: 21,
+    selected: false,
+  },
+  {
+    label: 'Attributable Deals',
+    value: 57,
+    formattedValue: '57',
+    percentage: 12.6,
+    selected: false,
+  },
+  {
+    label: 'Revenue',
+    value: 10622.21,
+    formattedValue: '$10,622.21',
+    percentage: 15.2,
+    selected: false,
+  },
+]
