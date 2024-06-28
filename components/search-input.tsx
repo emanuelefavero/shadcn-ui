85266831e3917fbd420d77c9b074aac0cf646623
emanuelefavero <emafaux89@gmail.com>
2024-06-28 16:36:10 +0200

    change search icon color

--- components/search-input.tsx
+++ components/search-input.tsx
@@ -10,15 +10,15 @@ function SearchInput() {
         <Input
           type='search'
           placeholder='Search...'
-          className='w-full pl-9 pr-4 py-2 bg-purple-900 xs:bg-opacity-5 placeholder:text-slate-500'
+          className='w-full pl-9 pr-4 py-2 bg-purple-900 bg-opacity-5 placeholder:text-slate-500'
         />
         <Search className='h-5 w-5 absolute left-2 top-2.5 text-slate-500' />
       </div>
 
       {/* Search Input Button */}
       <div className='block xs:hidden'>
-        <Button variant='outline' size='icon'>
-          <Search className='h-5 w-5 text-slate-500' />
+        <Button variant='outline' size='icon' className='hover:bg-transparent'>
+          <Search className='h-5 w-5 text-slate-500 hover:text-foreground' />
           <span className='sr-only'>Search</span>
         </Button>
       </div>
