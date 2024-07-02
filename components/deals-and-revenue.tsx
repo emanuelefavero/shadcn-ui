064dbaa23d6da15c8fe1b6eee7ef816f36d80bf9
emanuelefavero <emafaux89@gmail.com>
2024-07-02 14:38:43 +0200

    adjust chart position

--- components/deals-and-revenue.tsx
+++ components/deals-and-revenue.tsx
@@ -4,9 +4,9 @@ import { DealsAndRevenueChart } from './deals-and-revenue-chart'
 function DealsAndRevenue() {
   return (
     <>
-      <Card className='max-w-[500px] w-full rounded-xl overflow-hidden bg-purple-900 bg-opacity-5 py-6 px-5 flex flex-col justify-between'>
+      <Card className='max-w-[500px] w-full rounded-xl overflow-hidden bg-purple-900 bg-opacity-5 py-6 px-5 flex flex-col justify-between items-center'>
         <h2
-          className={`font-semibold text-xl text-text-secondary mb-2 select-none`}
+          className={`w-full font-semibold text-xl text-text-secondary mb-2 select-none`}
         >
           Deals and Revenue
         </h2>
