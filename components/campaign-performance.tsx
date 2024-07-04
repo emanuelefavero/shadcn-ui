32612ac2490245bb09a78fb6588759ab0e769ae0
emanuelefavero <emafaux89@gmail.com>
2024-07-04 07:18:11 +0200

    add hardcoded campaign data to campaign-performance component

--- components/campaign-performance.tsx
+++ components/campaign-performance.tsx
@@ -16,18 +16,33 @@ function CampaignPerformance() {
       <Table>
         <TableHeader>
           <TableRow>
-            <TableHead className='w-[100px]'>Invoice</TableHead>
-            <TableHead>Status</TableHead>
-            <TableHead>Method</TableHead>
-            <TableHead className='text-right'>Amount</TableHead>
+            <TableHead>NO.</TableHead>
+            <TableHead>CAMPAIGN</TableHead>
+            <TableHead>VISITORS</TableHead>
+            <TableHead>CONTACTS</TableHead>
+            <TableHead>COMPANIES</TableHead>
+            <TableHead>LEADS</TableHead>
+            <TableHead className='text-right'>VALUE</TableHead>
           </TableRow>
         </TableHeader>
         <TableBody>
           <TableRow>
-            <TableCell className='font-medium'>INV001</TableCell>
-            <TableCell>Paid</TableCell>
-            <TableCell>Credit Card</TableCell>
-            <TableCell className='text-right'>$250.00</TableCell>
+            <TableCell>1</TableCell>
+            <TableCell className='flex'>
+              <Avatar className='mr-3'>
+                <AvatarImage src='https://picsum.photos/id/82/50/?blur' />
+                <AvatarFallback>ROQ</AvatarFallback>
+              </Avatar>
+              <div className='flex flex-col'>
+                <span>ROQ</span>
+                <span>Search/brand</span>
+              </div>
+            </TableCell>
+            <TableCell>1,181</TableCell>
+            <TableCell>217</TableCell>
+            <TableCell>150</TableCell>
+            <TableCell>26.9</TableCell>
+            <TableCell className='text-right'>$78.29</TableCell>
           </TableRow>
         </TableBody>
       </Table>
