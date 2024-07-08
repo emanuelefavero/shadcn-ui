b0887b4b9cd205829d5c47878d05bfe986f1a94a
emanuelefavero <emafaux89@gmail.com>
2024-07-08 17:57:20 +0200

    add - justify center dashboard

--- components/dashboard.tsx
+++ components/dashboard.tsx
@@ -8,13 +8,13 @@ import { TopRevenue } from './top-revenue'
 function Dashboard() {
   return (
     <>
-      <section className='w-full flex justify-between'>
+      <section className='w-full flex justify-between mb-10'>
         <H1 className='font-semibold text-3xl mb-10 select-none'>Dashboard</H1>
         <SearchInput />
       </section>
 
       {/* Set the section to flex direction column on small screens and flex direction row on the others*/}
-      <section className='flex flex-row flex-wrap gap-8 pb-10'>
+      <section className='flex flex-row flex-wrap justify-center gap-8 pb-10'>
         <MainCard />
         <DealsAndRevenue />
         <CampaignPerformance />
