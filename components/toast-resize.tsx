fa72466d9a7cf5752e42c32165c90868a3624150
emanuelefavero <emafaux89@gmail.com>
2024-07-08 22:43:37 +0200

    add close button to toast resize

--- components/toast-resize.tsx
+++ components/toast-resize.tsx
@@ -1,24 +1,29 @@
 'use client'
 
-import React, { useState } from 'react'
+import { useState } from 'react'
 import useWindowResize from '@/hooks/useWindowResize'
+import { Card } from '@/components/ui/card'
 
 function ToastResize() {
   const [show, setShow] = useState(false)
 
   useWindowResize(() => {
     setShow(true)
-    setTimeout(() => setShow(false), 3000) // Hide after 3 seconds
+    setTimeout(() => setShow(false), 3000)
   })
 
   return (
-    <div
-      className={`fixed left-1/2 transform -translate-x-1/2 bg-gray-800 text-white py-3 px-6 rounded shadow-lg transition-all duration-500 z-50 ${
+    <Card
+      className={`fixed left-1/2 transform -translate-x-1/2 text-base text-foreground bg-purple-900 bg-opacity-5 pl-3 pr-6 py-2 rounded-lg transition-all duration-250 z-50 cursor-pointer hover:bg-rose-500/10 group ${
         show ? 'top-5' : '-top-20'
       }`}
+      onClick={() => setShow(false)}
     >
       <span>Refresh the page to resize the charts</span>
-    </div>
+      <span className='absolute top-0 right-0 px-2.5 py-1 text-sm text-rose-500 hidden group-hover:inline'>
+        X
+      </span>
+    </Card>
   )
 }
 
