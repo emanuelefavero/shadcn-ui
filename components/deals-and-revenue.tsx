9dcb03b8653e17da201e2ecb08150ed65626e86b
emanuelefavero <emafaux89@gmail.com>
2024-07-08 18:31:43 +0200

    fix deals and revenue small screens responsivness

--- components/deals-and-revenue.tsx
+++ components/deals-and-revenue.tsx
@@ -6,7 +6,7 @@ function DealsAndRevenue() {
     <>
       <Card className='max-w-[500px] w-full rounded-xl overflow-hidden bg-purple-900 bg-opacity-5 py-6 px-5 flex flex-col justify-between'>
         <h2
-          className={`font-semibold text-xl text-text-secondary mb-2 select-none`}
+          className={`font-semibold text-base 2xxs:text-xl text-text-secondary mb-2 select-none`}
         >
           Deals and Revenue
         </h2>
