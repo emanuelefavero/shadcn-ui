2220bbedee6f7ffceb12fd1bbedb9620123df649
emanuelefavero <emafaux89@gmail.com>
2024-07-08 18:46:35 +0200

    fix top revenue small screens responsivness

--- components/top-revenue.tsx
+++ components/top-revenue.tsx
@@ -8,10 +8,10 @@ import './top-revenue.css'
 function TopRevenue() {
   return (
     <div className='w-fit flex justify-center'>
-      <Card className='dottedOutsideBorder relative max-w-[320px] max-h-[380px] w-full rounded-xl bg-gradient-to-tr from-[rgba(182,27,243,0.1)] dark:from-[rgba(182,27,243,0.24)] to-transparent py-5 px-5 flex flex-col justify-between'>
+      <Card className='dottedOutsideBorder relative max-w-[190px] 2xxs:max-w-[320px] 2xxs:max-h-[380px] 2xxs:w-full rounded-xl bg-gradient-to-tr from-[rgba(182,27,243,0.1)] dark:from-[rgba(182,27,243,0.24)] to-transparent py-5 px-5 flex flex-col justify-between'>
         <div className='w-full flex justify-between items-center mb-6'>
           <h2
-            className={`font-semibold text-xl text-text-secondary select-none`}
+            className={`font-semibold text-base 2xxs:text-xl text-text-secondary select-none`}
           >
             Top Revenue Channels
           </h2>
@@ -23,7 +23,7 @@ function TopRevenue() {
         <TopRevenueChart />
 
         {/* Center span inside card */}
-        <span className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-2xl mt-[0.9rem] tracking-wider font-semibold text-text-primary select-none'>
+        <span className='hidden 2xxs:block absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-2xl mt-[0.9rem] tracking-wider font-semibold text-text-primary select-none'>
           $4,271
         </span>
       </Card>
