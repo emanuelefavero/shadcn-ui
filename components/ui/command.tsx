5787ce0d9821dfdb8cdaea06e450f3b38b484502
emanuelefavero <emafaux89@gmail.com>
2024-07-10 22:13:17 +0200

    fix command dialog warning

--- components/ui/command.tsx
+++ components/ui/command.tsx
@@ -1,7 +1,12 @@
 'use client'
 
 import * as React from 'react'
-import { type DialogProps } from '@radix-ui/react-dialog'
+import {
+  DialogDescription,
+  DialogTitle,
+  type DialogProps,
+} from '@radix-ui/react-dialog'
+import * as VisuallyHidden from '@radix-ui/react-visually-hidden'
 import { Command as CommandPrimitive } from 'cmdk'
 import { Search } from 'lucide-react'
 
@@ -29,6 +34,14 @@ const CommandDialog = ({ children, ...props }: CommandDialogProps) => {
   return (
     <Dialog {...props}>
       <DialogContent className='overflow-hidden p-0 shadow-lg rounded-lg'>
+        <DialogTitle>
+          <VisuallyHidden.Root>Command Dialog</VisuallyHidden.Root>
+        </DialogTitle>
+        <DialogDescription>
+          <VisuallyHidden.Root>
+            Use the up and down keys to navigate
+          </VisuallyHidden.Root>
+        </DialogDescription>
         <Command className='[&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:font-medium [&_[cmdk-group-heading]]:text-muted-foreground [&_[cmdk-group]:not([hidden])_~[cmdk-group]]:pt-0 [&_[cmdk-group]]:px-2 [&_[cmdk-input-wrapper]_svg]:h-5 [&_[cmdk-input-wrapper]_svg]:w-5 [&_[cmdk-input]]:h-12 [&_[cmdk-item]]:px-2 [&_[cmdk-item]]:py-3 [&_[cmdk-item]_svg]:h-5 [&_[cmdk-item]_svg]:w-5'>
           {children}
         </Command>
