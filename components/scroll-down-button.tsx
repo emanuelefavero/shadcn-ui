f78283ca967d5ffcb8d6cf94b80ec26cbe62ea15
emanuelefavero <emafaux89@gmail.com>
2024-07-12 06:16:11 +0200

    add scroll down chrevon icon

--- components/scroll-down-button.tsx
+++ components/scroll-down-button.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { ChevronDown } from 'lucide-react'
 import { useScrollDistance } from '@/hooks/useScrollDistance'
 
 function ScrollDownButton() {
@@ -8,8 +9,8 @@ function ScrollDownButton() {
   if (scrollDistance > 130) return null
 
   return (
-    <div className='block lg:hidden absolute bottom-3 right-4 z-50 text-3xl font-medium text-purple-400 text-opacity-50 dark:text-purple-600 dark:text-opacity-50 select-none animate-bounce'>
-      ↓
+    <div className='block lg:hidden absolute bottom-3 left-1/2 z-50 text-3xl font-medium text-purple-400 text-opacity-50 dark:text-purple-600 dark:text-opacity-50 select-none animate-bounce'>
+      <ChevronDown className='w-10 h-10' />
     </div>
   )
 }
